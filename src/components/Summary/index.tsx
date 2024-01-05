9ac5b9d18bceb52be33fc6f328d824d78ce9a560
Victor Muniz <victorrmunizz@gmail.com>
2024-01-05 17:43:56 -0300

    creating structure

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,15 @@
+import { SummaryContainer } from "./styles";
+
+export function Summary() {
+    return (
+        <div>
+           <SummaryContainer>
+                <header>
+                    <span>Entradas</span>
+                </header>
+
+
+           </SummaryContainer>
+        </div>
+    )
+}
